e3697f495782bdf3c09c8d3376c8f72910895fab
Kevin Céssar <102882654+KevinCesarDev@users.noreply.github.com>
2023-04-28 17:15:32 -0300

    Create script.js

---
+++ WEB/web_site_html_css_java/js/script.js
@@ -0,0 +1,12 @@
+
+var loginValido = "Kevin";
+var senhaValida = "123456";
+
+var loginUsuario = document.getElementById(login);
+var senhaUsuario = document.getElementById(senha);
+
+function validarLogin(){
+    if(loginUsuario == "" && senhaUsuario == ""){
+        
+    }
+}
