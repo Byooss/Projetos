8851d7f65e95882a86ec5b9a9eed1e451d723e7e
Kevin Céssar <102882654+KevinCesarDev@users.noreply.github.com>
2023-04-29 00:03:46 -0300

    v.0.1

--- WEB/web_site_html_css_java/js/script.js
+++ WEB/web_site_html_css_java/js/script.js
@@ -2,11 +2,33 @@
 var loginValido = "Kevin";
 var senhaValida = "123456";
 
-var loginUsuario = document.getElementById(login);
-var senhaUsuario = document.getElementById(senha);
 
-function validarLogin(){
-    if(loginUsuario == "" && senhaUsuario == ""){
-        
+
+function validarLogin() {
+    var loginUsuario = document.getElementById("login").value;
+    var senhaUsuario = document.getElementById("senha").value;
+    let msgErro = document.getElementById("campoErro");
+
+    if ((loginUsuario == null && senhaUsuario == null)) {
+        document.getElementById("campoErro").innerHTML = "Nenhum dos campos foi preenchido";
+    }
+    if (loginUsuario == loginValido && senhaUsuario != senhaValida) {
+        document.getElementById("campoErro").innerHTML = "Senha inválida";
+    }
+    if (loginUsuario != loginValido && senhaUsuario == senhaValida) {
+        document.getElementById("campoErro").innerHTML = "Usuário inválido";
+    }
+    if (loginUsuario != loginValido && senhaUsuario != senhaValida) {
+        document.getElementById("campoErro").innerHTML = "Usuário e senha inválidos"
+    }
+    if (loginUsuario == loginValido && senhaUsuario == senhaValida) {
+        document.getElementById("campoErro").innerHTML = "Login realizado com sucesso"
+        setTimeout(function () {
+            window.location.href = "perfil.html"
+        }, 2000);
     }
 }
+
+
+
+
